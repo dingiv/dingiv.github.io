bf85fb0b5dca71982d70a5b03f8588919182abf1
dingiv <dingiv@users.noreply.github.com>
2025-08-20 15:21:19 +0000

    deploy: 42b9099068f2811cdd8b766edcc73b977b554390

---
+++ assets/basic_db_nf.md.DWRdqnaj.lean.js
@@ -0,0 +1,1 @@
+import{_ as i,c as a,o as n,ae as l}from"./chunks/framework.Cd-3tpCq.js";const g=JSON.parse('{"title":"范式","description":"","frontmatter":{},"headers":[],"relativePath":"basic/db/nf.md","filePath":"basic/db/nf.md"}'),h={name:"basic/db/nf.md"};function p(k,s,t,e,E,r){return n(),a("div",null,s[0]||(s[0]=[l("",27)]))}const y=i(h,[["render",p]]);export{g as __pageData,y as default};
